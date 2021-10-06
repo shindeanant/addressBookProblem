e0bebf50b7fb95e89a0d9ddfae7fc28eb3be9592
shindeanant <shindeanant1997@gmail.com>
2021-10-07 00:54:24 +0530

    ADD: Edit new contact deatils

--- NewContact.java
+++ NewContact.java
@@ -9,7 +9,6 @@ import org.apache.logging.log4j.Logger;
 public class NewContact {
 
 	private static final Logger LOG = LogManager.getLogger("BookAddress.class");
-
 	Integer[] phoneNumber = new Integer[10];
 	String[] Address = new String[10];
 	String[] Firstname = new String[10];
@@ -17,7 +16,6 @@ public class NewContact {
 
 	public void getValue(int phonenumber, String address, String firstname, String lastname, int length, int time) {
 		int count = 0;
-
 		for (int i = time; i < length; i++) {
 			phoneNumber[i] = phonenumber;
 			Address[i] = address;
@@ -25,18 +23,27 @@ public class NewContact {
 			Lastname[i] = lastname;
 			// count=count+1;
 		}
-
 		LOG.info("phoneNumber" + Arrays.toString(phoneNumber));
 		LOG.info("Address" + Arrays.toString(Address));
 		LOG.info("Firstname" + Arrays.toString(Firstname));
 		LOG.info("Lastname" + Arrays.toString(Lastname));
-
 	}
 
+	public void replace(String name, String newname) {
+
+		boolean result = (Arrays.asList(Firstname).contains(name));
+		if (result == true) {
+			Firstname[0] = newname;
+		}
+		// boolean contains = Arrays.stream(Firstname).anyMatch("name"::equals);
+		// for(int i=0;i<10;i++)
+		// {
+		// }
+		LOG.info("Firstname" + Arrays.toString(Firstname));
+	}
 }
 
 class BookAddress extends NewContact {
-
 	private static final Logger LOG = LogManager.getLogger("BookAddress.class");
 	private static final String NULL = null;
 
@@ -45,15 +52,13 @@ class BookAddress extends NewContact {
 		LOG.info("Welcome to address Book Program");
 
 		Scanner value = new Scanner(System.in);
-
 		BookAddress contact = new BookAddress();
-
+		LOG.info("Press 1 for ADD, press 2 for REPLACE and press 3 for DELETE");
 		// LOG.info("1.ADD 2.REPLACE 3.DELETE");
 		// 1LOG.info("enter Your Choice");
 		int Check = value.nextInt();
 		int choice;
 		do {
-			LOG.info("Press 1 for ADD, press 2 for REPLACE and press 3 for DELETE");
 
 			LOG.info("Enter Your Choice: ");
 
@@ -80,7 +85,17 @@ class BookAddress extends NewContact {
 				}
 				// contact.getValue(phonenumber,address,firstname,lastname);
 				break;
+			case 2:
+				Scanner sc = new Scanner(System.in);
+				LOG.info("Enter the name you want to rename");
+				String name = sc.nextLine();
 
+				Scanner next = new Scanner(System.in);
+				LOG.info("Enter the new name");
+				String newname = next.nextLine();
+
+				contact.replace(name, newname);
+				break;
 			}
 			LOG.info("Do you want to continue Then press 1 for yes and press 2 for No");
 			choice = value.nextInt();
