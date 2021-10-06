05f564271c7512872f62bcbbf74d69ea76314f31
shindeanant <shindeanant1997@gmail.com>
2021-10-07 00:04:57 +0530

    ADD: Contact Details Created.

--- AddressBook.java
+++ AddressBook.java
@@ -1,5 +1,7 @@
 package com.bridgelabzz.bookaddress;
 
+import java.util.Scanner;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,5 +11,24 @@ public class AddressBook {
 	public static void main(String[] args) {
 
 		LOG.info("Wecome to address Book Program");
+		int PhoneNumber;
+		String Address;
+		String FirstName;
+		String lastName;
+
+		Scanner value = new Scanner(System.in);
+		LOG.info("Enter the first name");
+		FirstName = value.nextLine();
+		LOG.info("Enter the last name");
+		lastName = value.nextLine();
+		LOG.info("Enter the Address name");
+		Address = value.nextLine();
+		LOG.info("Enter the phone number");
+		PhoneNumber = value.nextInt();
+
+		LOG.info("fistName" + FirstName);
+		LOG.info("LastName" + lastName);
+		LOG.info("address" + Address);
+		LOG.info("PhoneNumber" + PhoneNumber);
 	}
 }
