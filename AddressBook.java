679063f89b2814a3a80e3477fb11ef44d9f0d3fa
shindeanant <shindeanant1997@gmail.com>
2021-10-06 23:56:08 +0530

    ADD: Welcome message

---
+++ AddressBook.java
@@ -0,0 +1,13 @@
+package com.bridgelabzz.bookaddress;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class AddressBook {
+	private static final Logger LOG = LogManager.getLogger("App.class");
+
+	public static void main(String[] args) {
+
+		LOG.info("Wecome to address Book Program");
+	}
+}
